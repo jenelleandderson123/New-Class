e2174b841ed7b365d5510c9f985e0f1f65877a94
jenelleandderson123 <jenelleannderson@gmail.com>
2025-10-21 19:21:35 +0000

    Add files via upload

---
+++ var.py
@@ -0,0 +1,11 @@
+birth1="3rd December"
+birth2="14th April"
+birth3="25th December"
+birth4="21st October"
+birth5="13th January"
+
+print(birth1)
+print(birth2)
+print(birth3)
+print(birth4)
+print(birth5)
