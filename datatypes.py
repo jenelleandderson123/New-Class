cdc01ebc912d549a100c0f3c6375758eb46b1b64
jenelleandderson123 <jenelleannderson@gmail.com>
2025-10-23 17:54:38 +0000

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,17 @@
+a = 56
+b= "codingal"
+c = 32.5
+d = True
+x =[ "coding", "programming","python", "HTML"]
+
+y = {"coding", "programming","python", "HTML"}
+
+z = ("coding", "programming","python", "HTML")
+
+print ("type a :", type(a))
+print ("type b :", type(b))
+print ("type c :", type(c) )
+print ("type d :", type(d) )
+print ("type x :", type(x) )
+print ("type y :", type(y) )
+print ("type z :", type(z) )
