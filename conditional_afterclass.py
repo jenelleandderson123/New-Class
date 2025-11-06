f0774de7f24850f86ccad296414bd4d4bf212923
jenelleandderson123 <jenelleannderson@gmail.com>
2025-11-06 20:03:07 +0000

    Add files via upload

---
+++ conditional_afterclass.py
@@ -0,0 +1,8 @@
+gender = input("Are you a girl? (yes or no): ")
+
+if gender == "yes":
+    print("Okay pretty queen")
+elif gender == "no":
+    print("Alright bro, how was the football game.")
+else:
+    print("Please be specific")
