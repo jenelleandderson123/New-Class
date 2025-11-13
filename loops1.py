ad62dd3703f637edf5140f28b733237dc68f3086
ex_baby ese <jenelleannderson@gmail.com>
2025-11-13 17:55:11 +0000

    Add files via upload

---
+++ loops1.py
@@ -0,0 +1,7 @@
+num = int(input("Please enter the number whose sum you want to find"))
+sum = 0
+
+for i in range(1, num+1):
+   sum = sum + 1
+   print("\n sum = ", sum) 
+
---
+++ loops2.py
@@ -0,0 +1,9 @@
+string = input("Please enter your string")
+
+string2 = ('' )
+
+for i in string:
+    string2 = i + string2
+
+print("\n The original string is ", string)
+print("The reversed string is", string2)
